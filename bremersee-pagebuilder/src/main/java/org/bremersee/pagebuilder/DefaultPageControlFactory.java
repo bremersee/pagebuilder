b27f8b574477b858e782175e408f12573d93d3f0
Christian Bremer <christian.bremersee.org>
2015-12-06 08:20:48 +0100

    update

---
+++ bremersee-pagebuilder/src/main/java/org/bremersee/pagebuilder/DefaultPageControlFactory.java
@@ -0,0 +1,230 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bremersee.pagebuilder;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.TreeSet;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Validate;
+import org.bremersee.comparator.model.ComparatorItem;
+import org.bremersee.pagebuilder.model.PageControl;
+import org.bremersee.pagebuilder.model.PageControl.MaxResultsSelectorOption;
+import org.bremersee.pagebuilder.model.PageControl.Pagination;
+import org.bremersee.pagebuilder.model.PageControl.PaginationButton;
+import org.bremersee.pagebuilder.model.PageDto;
+import org.bremersee.utils.WebUtils;
+
+/**
+ * <p>
+ * The default page control factory.
+ * </p>
+ * 
+ * @author Christian Bremer
+ */
+class DefaultPageControlFactory extends PageControlFactory {
+
+    public PageControl newPageControl(
+			PageDto page, 
+			String pageUrl, 
+			String query, 
+			Locale locale) {
+		
+		return buildPageControl(page, pageUrl, query, locale);
+	}
+	
+	protected PageControl buildPageControl(
+            PageDto page, 
+            String pageUrl, 
+            String query, 
+            Locale locale) {
+
+		Validate.notNull(page, "page must not be null");
+		if (page.getMaxResults() == null || page.getMaxResults() < 1) {
+		    page.setMaxResults(Integer.MAX_VALUE);
+		}
+		
+		if (locale == null) {
+			locale = Locale.getDefault();
+		}
+		
+		PageControl pageControl = new PageControl();
+		
+		pageControl.setComparatorParamName(getComparatorParamName());
+		pageControl.setMaxResultsParamName(getMaxResultsParamName());
+		pageControl.setPageNumberParamName(getPageNumberParamName());
+		pageControl.setQueryParamName(getQueryParamName());
+		
+		pageControl.setPage(page);
+		
+		pageControl.setComparatorParamValue(getComparatorItemTransformer().toString(page.getComparatorItem(), false, null));
+		
+		pageControl.setMaxResultsSelectorOptions(buildMaxResultsSelectorOptions(
+		        page.getMaxResults(), 
+		        locale));
+		
+		pageControl.setPagination(buildPagination(page, pageUrl, query));
+		
+		pageControl.setQuerySupported(isQuerySupported());
+		pageControl.setQuery(query);
+		
+		return pageControl;
+	}
+
+	protected String buildUrl(String url, int pageNo, int maxResults, ComparatorItem comparatorItem, String query) {
+		
+		String newUrl = WebUtils.addUrlParameter(url, getPageNumberParamName(), Integer.valueOf(pageNo).toString());
+		newUrl = WebUtils.addUrlParameter(newUrl, getMaxResultsParamName(), Integer.valueOf(maxResults).toString());
+		String comparatorParamValue = getComparatorItemTransformer().toString(comparatorItem, false, null);
+		if (StringUtils.isNotBlank(comparatorParamValue)) {
+			newUrl = WebUtils.addUrlParameter(newUrl, getComparatorParamName(), comparatorParamValue);
+		}
+		if (StringUtils.isNotBlank(query)) {
+			newUrl = WebUtils.addUrlParameter(newUrl, getQueryParamName(), query);
+		}
+		return newUrl;
+	}
+	
+	protected Pagination buildPagination(
+			PageDto page, 
+			String pageUrl, 
+			String query) {
+		
+		int maxPaginationButtons = getMaxPaginationButtons();
+		
+		if (maxPaginationButtons < 1) {
+			maxPaginationButtons = 1;
+		}
+		
+		Validate.notEmpty(pageUrl, "pageUrl must not be empty");
+		Validate.notNull(page.getPageSize(), "page.getPageSize() must not be null");
+		Validate.notNull(page.getCurrentPage(), "page.getCurrentPage() must not be null");
+		
+		if (page.getPageSize() < maxPaginationButtons) {
+			maxPaginationButtons = page.getPageSize();
+		}
+		
+		Pagination pagination = new Pagination();
+		pagination.setMaxPaginationButtons(maxPaginationButtons);
+		
+		for (int pageNumber = 0; pageNumber < page.getPageSize(); pageNumber++) {
+			
+			PaginationButton paginationEntry = new PaginationButton();
+			paginationEntry.setActive(pageNumber == page.getCurrentPage());
+			paginationEntry.setPageNumber(pageNumber);
+			paginationEntry.setUrl(buildUrl(pageUrl, pageNumber, page.getMaxResults(), page.getComparatorItem(), query));
+			
+			pagination.getAllButtons().add(paginationEntry);
+		}
+		
+		int pageNumberOfFirstButton;
+		if (maxPaginationButtons % 2 == 0) {
+			pageNumberOfFirstButton = page.getCurrentPage() - maxPaginationButtons / 2 + 1;
+		} else {
+			int middle = Double.valueOf(Math.floor(maxPaginationButtons / 2.)).intValue();
+			pageNumberOfFirstButton = page.getCurrentPage() - middle;
+		}
+		if (pageNumberOfFirstButton < 0) {
+			pageNumberOfFirstButton = 0;
+		}
+		for (int pageNumber = pageNumberOfFirstButton; pageNumber < pageNumberOfFirstButton + maxPaginationButtons && pageNumber < page.getPageSize(); pageNumber++) {
+			pagination.getButtons().add(pagination.getAllButtons().get(pageNumber));
+		}
+		
+		boolean firstPageDisabled = page.getCurrentPage() == 0;
+		String firstPageUrl = firstPageDisabled ? "#" : buildUrl(pageUrl, 0, page.getMaxResults(), page.getComparatorItem(), query);
+		PaginationButton firstPage = new PaginationButton(0, !firstPageDisabled, firstPageUrl);
+		pagination.setFirstPageButton(firstPage);
+		
+		boolean previousPageDisabled = page.getCurrentPage() == 0;
+		String previousPageUrl = previousPageDisabled ? "#" : buildUrl(pageUrl, page.getCurrentPage()-1, page.getMaxResults(), page.getComparatorItem(), query);
+		PaginationButton previousPage = new PaginationButton(page.getCurrentPage()-1, !previousPageDisabled, previousPageUrl);
+		pagination.setPreviousPageButton(previousPage);
+		
+		boolean nextPageDisabled = page.getCurrentPage() == page.getPageSize()-1;
+		String nextPageUrl = nextPageDisabled ? "#" : buildUrl(pageUrl, page.getCurrentPage()+1, page.getMaxResults(), page.getComparatorItem(), query);
+		PaginationButton nextPage = new PaginationButton(page.getCurrentPage()+1, !nextPageDisabled, nextPageUrl);
+		pagination.setNextPageButton(nextPage);
+		
+		boolean lastPageDisabled = page.getCurrentPage() == page.getPageSize()-1;
+		String lastPageUrl = lastPageDisabled ? "#" : buildUrl(pageUrl, page.getPageSize()-1, page.getMaxResults(), page.getComparatorItem(), query);
+		PaginationButton lastPage = new PaginationButton(page.getPageSize()-1, !lastPageDisabled, lastPageUrl);
+		pagination.setLastPageButton(lastPage);
+		
+		return pagination;
+	}
+
+	protected List<MaxResultsSelectorOption> buildMaxResultsSelectorOptions(
+			int selectedMaxResults, 
+			Locale locale) {
+		
+		final int maxResultsSelectorMinValue = getMaxResultsSelectorMinValue();
+		final int maxResultsSelectorMaxValue = getMaxResultsSelectorMaxValue();
+		final int maxResultsSelectorStep = getMaxResultsSelectorStep();
+		
+		Validate.isTrue(0 < maxResultsSelectorMinValue && maxResultsSelectorMinValue <= maxResultsSelectorMaxValue, 
+				"0 < maxResultsSelectorMinValue && maxResultsSelectorMinValue <= maxResultsSelectorMaxValue must be 'true'");
+		
+		if (maxResultsSelectorMaxValue > maxResultsSelectorMinValue) {
+			Validate.isTrue(maxResultsSelectorStep > 0, "maxResultsSelectorStep > 0 must be 'true'");
+		}
+		
+		Validate.isTrue(selectedMaxResults > 0, "selectedMaxResults > 0 must be 'true'");
+		
+		if (locale == null) {
+			locale = Locale.getDefault();
+		}
+		
+		boolean selctedValueAdded = false;
+		TreeSet<MaxResultsSelectorOption> options = new TreeSet<MaxResultsSelectorOption>();
+		for (int i = maxResultsSelectorMinValue; i <= maxResultsSelectorMaxValue; i = i + maxResultsSelectorStep) {
+			if (i == selectedMaxResults) {
+				options.add(new MaxResultsSelectorOption(i, Integer.valueOf(i).toString(), true));
+				selctedValueAdded = true;
+			} else {
+				options.add(new MaxResultsSelectorOption(i, Integer.valueOf(i).toString(), false));
+			}
+		}
+		
+		if (isSelectAllResultsAvailable()) {
+			
+			ResourceBundle resourceBundle = ResourceBundle.getBundle(RESOURCE_BUNDLE_BASE_NAME, locale); 
+			String displayedValue;
+			if (resourceBundle.containsKey("pageBuilderFactory.maxResultsSelector.max")) {
+				displayedValue = resourceBundle.getString("pageBuilderFactory.maxResultsSelector.max");
+			} else {
+				displayedValue = "Max";
+			}
+			
+			if (Integer.MAX_VALUE == selectedMaxResults) {
+				options.add(new MaxResultsSelectorOption(Integer.MAX_VALUE, displayedValue, true));
+				selctedValueAdded = true;
+			} else {
+				options.add(new MaxResultsSelectorOption(Integer.MAX_VALUE, displayedValue, false));
+			}
+		}
+		if (!selctedValueAdded) {
+			options.add(new MaxResultsSelectorOption(selectedMaxResults, Integer.valueOf(selectedMaxResults).toString(), true));
+		}
+		return Collections.unmodifiableList(new ArrayList<MaxResultsSelectorOption>(options));
+	}
+	
+}
--- bremersee-pagebuilder/src/main/java/org/bremersee/pagebuilder/PageBuilder.java
+++ bremersee-pagebuilder/src/main/java/org/bremersee/pagebuilder/PageBuilder.java
@@ -26,60 +26,87 @@ import org.bremersee.pagebuilder.model.PageDto;
  * <p>
  * A page builder creates a {@link PageDto} form a list of items.
  * </p>
+ * <p>
+ * There are two ways to create a page:
+ * <ul>
+ * <li>The list of items that build the page is pre-filtered.<br/>
+ * The list of items is perhaps the result of a SQL query.<br/>
+ * Than the page must build with
+ * {@link PageBuilder#buildPage(Collection, Integer, Integer, Integer, ComparatorItem)}
+ * .</li>
+ * <li>The list of items that build the page is not filtered and contains all
+ * available items.<br/>
+ * Than the page must build with
+ * {@link PageBuilder#buildFilteredPage(Collection, Integer, Integer, ObjectComparator, Object)}
+ * .</li>
+ * </ul>
+ * </p>
  * 
  * @author Christian Bremer
- *        
+ * 
  */
 public interface PageBuilder {
 
-    /**
-     * Creates a page with the specified parameters. The entries will not be
-     * filtered nor sorted.
-     * 
-     * @param entries
-     *            the entries on the page
-     * @param firstResult
-     *            the number of the first entry of all available entries
-     * @param maxResults
-     *            the maximum number of entries (
-     *            {@code size of entries <= maxResults}
-     * @param totalSize
-     *            the size of all available entries
-     * @param comparatorItem
-     *            the comparator item
-     * @return the page with specified parameters
-     */
-    PageDto buildPage(Collection<? extends Object> entries, Integer firstResult, Integer maxResults, Integer totalSize,
+	/**
+	 * Creates a page with the specified parameters. The entries will not be
+	 * filtered nor sorted.
+	 * 
+	 * @param entries
+	 *            the entries on the page
+	 * @param firstResult
+	 *            the number of the first entry of all available entries
+	 * @param maxResults
+	 *            the maximum number of entries (
+	 *            {@code size of entries <= maxResults}
+	 * @param totalSize
+	 *            the size of all available entries
+	 * @param comparatorItem
+	 *            the comparator item
+	 * @return the page with specified parameters
+	 */
+	//@formatter:off
+    PageDto buildPage(
+    		Collection<? extends Object> entries, 
+    		Integer firstResult, 
+    		Integer maxResults, 
+    		Integer totalSize,
             ComparatorItem comparatorItem);
+	//@formatter:on
 
-    /**
-     * Creates a page with the specified parameters. The entries will be
-     * filtered:
-     * <ul>
-     * <li>First sort the entries, if an object comparator is present.</li>
-     * <li>Than get all accepted items, if a filter is present.</li>
-     * <li>Add {@code maxResults} items from {@code firstResult} position to the
-     * page.</li>
-     * <li>The total size attribute of the page is the size of the specified
-     * entries.</li>
-     * </ul>
-     * 
-     * @param entries
-     *            all available entries that should be filtered by
-     *            {@code firstResult}, {@code maxResults} and {@code filter},
-     *            the total size is the size of the entries
-     * @param firstResult
-     *            the number of the first entry of the specified entries
-     * @param maxResults
-     *            the maximum number of entries (
-     *            {@code size of page <= maxResults})
-     * @param objectComparator
-     *            an object comparator (optional)
-     * @param filter
-     *            a filter to get accepted items (optional)
-     * @return the page with filtered entries
-     */
-    PageDto buildFilteredPage(Collection<? extends Object> entries, Integer firstResult, Integer maxResults,
-            ObjectComparator objectComparator, Object filter);
+	/**
+	 * Creates a page with the specified parameters. The entries will be
+	 * filtered:
+	 * <ul>
+	 * <li>First sort the entries, if an object comparator is present.</li>
+	 * <li>Than get all accepted items, if a filter is present.</li>
+	 * <li>Add {@code maxResults} items from {@code firstResult} position to the
+	 * page.</li>
+	 * <li>The total size attribute of the page is the size of the specified
+	 * entries.</li>
+	 * </ul>
+	 * 
+	 * @param entries
+	 *            all available entries that should be filtered by
+	 *            {@code firstResult}, {@code maxResults} and {@code filter},
+	 *            the total size is the size of the entries
+	 * @param firstResult
+	 *            the number of the first entry of the specified entries
+	 * @param maxResults
+	 *            the maximum number of entries (
+	 *            {@code size of page <= maxResults})
+	 * @param objectComparator
+	 *            an object comparator (optional)
+	 * @param filter
+	 *            a filter to get accepted items (optional)
+	 * @return the page with filtered entries
+	 */
+	//@formatter:off
+    PageDto buildFilteredPage(
+    		Collection<? extends Object> entries, 
+    		Integer firstResult,
+    		Integer maxResults,
+            ObjectComparator objectComparator, 
+            Object filter);
+	//@formatter:on
 
 }
